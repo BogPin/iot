97dce9206b49cae1aa78bc6fd35df11b2a830b5d
BogPin <bogdanpin2002@gmail.com>
2024-04-12 00:21:47 +0300

    Lab4

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -12,4 +12,13 @@ def process_agent_data(
     Returns:
         processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
     """
-    # Implement it
+    process_data = ProcessedAgentData(road_state="unknown", agent_data=agent_data)
+
+    if agent_data.accelerometer.y < -150:
+        process_data.road_state = "pit"
+    elif agent_data.accelerometer.y > 150:
+        process_data.road_state = "hill"
+    else:
+        process_data.road_state = "flat"
+
+    return process_data
